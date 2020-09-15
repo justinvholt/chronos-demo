4a6cbf69ebff6abeb13350d9af9e6d8a0d3c7c78
Justin Holt <justinvholt@gmail.com>
2020-09-15 00:30:44 -0500

    Streamlined demo version by forgoing devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :definitions]
+  # skip_before_action :authenticate_user!, only: [:home, :definitions]
 
   def home
     if user_signed_in?
